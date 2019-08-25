2364394f4153cc41c1e71140fa82336bdea06259
karinlinux <31069662+karinlinux@users.noreply.github.com>
2019-08-25 15:59:04 +0430

    First commit

---
+++ jsonToDot.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import json
+import sys
+
+
+def extract(json_obj, pat):
+    if type(json_obj) == type(dict()):
+        for key in json_obj.keys():
+            extract(json_obj[key], pat + key + '.')
+    elif type(json_obj) == type(list()):
+        for item in json_obj:
+            extract(item, pat)
+    else:
+        print(pat.strip('.'))
+
+
+if __name__ == '__main__':
+
+    input_file = sys.argv[1]
+    with open(input_file, 'r') as f:
+        json_file = json.load(f)
+        extract(json_file, "")
